0a9dbfe645973d24d512e5d219c5220a6b247836
bilgeadammelike <bilgeadammelike@mynet.com>
2019-05-24 12:45:59 +0300

    Add project files.

---
+++ TcpApp.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpApp.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1", 5555);
+
+            Console.WriteLine("Client Başlatıldı");
+            NetworkStream ns = client.GetStream();
+
+            Console.WriteLine("mesaj giriniz.. ");
+            string gonderilecekClientMesajı = Console.ReadLine();
+
+            byte[] writeBytes = Encoding.UTF8.GetBytes(gonderilecekClientMesajı);
+
+            ns.Write(writeBytes, 0, writeBytes.Length);
+
+            byte[] gelenServerMesaji = new byte[client.ReceiveBufferSize];
+            ns.Read(gelenServerMesaji, 0, gelenServerMesaji.Length);
+
+            Console.WriteLine("Server:" + Encoding.UTF8.GetString(gelenServerMesaji));
+
+        }
+    }
+}
---
+++ TcpApp.Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpApp.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TcpListener server = new TcpListener(5555);
+            server.Start();
+
+            Console.WriteLine("server başlatıldı..");
+
+            TcpClient getClients = server.AcceptTcpClient();
+
+            NetworkStream ns = getClients.GetStream();
+
+            //byte[] gonderilmisDeger =Encoding.UTF8.GetBytes()
+            byte[] deger = new byte[getClients.ReceiveBufferSize];
+
+            ns.Read(deger, 0, deger.Length);
+
+            Console.WriteLine("Gelen Mesaj :" + Encoding.UTF8.GetString(deger));
+
+            string donulecekDeger = "mesajınız alındı..";
+
+            byte[] gonderilecekDeger = Encoding.UTF8.GetBytes(donulecekDeger);
+            ns.Write(gonderilecekDeger, 0, gonderilecekDeger.Length);
+
+
+
+        }
+    }
+}
